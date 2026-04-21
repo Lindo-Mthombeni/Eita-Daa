15271b4ff1a150bb0f109aa9764b5f15340e66aa
Lindo-Mthombeni <ntsakomthombeni13@gmail.com>
2026-04-21 17:56:15 +0200

    Update Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Route, Routes } from "react-router-dom";
-import { Home } from "@pages/Home";
-import { NavSection } from "@pages/NavSection";
-import "./App.css";
-import { SignUp } from "@pages/SignUp";
-import { LogIn } from "@pages/LogIn";
-import { useNav } from "@lib/NavContext";
-import { SideBar } from "@pages/SideBar";
-import { LandingPage } from "@landing-page/LandingPage";
+// import { Route, Routes } from "react-router-dom";
+// import { Home } from "@pages/Home";
+// import { NavSection } from "@pages/NavSection";
+// import "./App.css";
+// import { SignUp } from "@pages/SignUp";
+// import { LogIn } from "@pages/LogIn";
+// import { useNav } from "@lib/NavContext";
+// import { SideBar } from "@pages/SideBar";
+// import { LandingPage } from "@landing-page/LandingPage";
 
 function App() {
-  const { menuOpen } = useNav();
+  // const { menuOpen } = useNav();
 
   return (
     <>
-      <NavSection />
+    <h1>Hello</h1>
+      {/* <NavSection />
       {menuOpen && <SideBar />}
       <main className="min-h-[calc(100vh-var(--spacing-nav))] pt-nav">
         <Routes>
@@ -23,7 +24,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="*" element={<h1>404</h1>} />
         </Routes>
-      </main>
+      </main> */}
     </>
   );
 }
